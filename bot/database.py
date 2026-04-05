aec19ecb3607e7bb185f18de74e6ecf4dc885276
Denis <denis.zrazh@gmail.com>
2026-04-05 20:52:01 +0500

    added file saving to SUPABASE

--- bot/database.py
+++ bot/database.py
@@ -12,7 +12,7 @@ engine = create_async_engine(
     DATABASE_URL,
     echo=False,
     json_serializer=lambda obj: json.dumps(obj, ensure_ascii=False),
-    # connect_args={"ssl": "require"},
+    connect_args={"ssl": "require"},
 )
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
